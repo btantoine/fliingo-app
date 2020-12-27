996a1a98aa4412ffe20afc3539c9e17a12b6b5fd
antoine <antoine.boudet@epitech.eu>
2020-12-27 19:52:05 +0100

    add french and english language

--- app.js
+++ app.js
@@ -1,43 +1,195 @@
-const intro = document.querySelector(".intro");
-const video = intro.querySelector("video");
-const text = intro.querySelector("h1");
-//END SECTION
-const section = document.querySelector("section");
-const end = section.querySelector("h1");
+// define images
+if (window.innerWidth > 992) {
+  var images = [
+    "rotato_frames_compressed/animated-landing-page-video Frame 1-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 2-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 3-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 4-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 5-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 6-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 7-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 8-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 9-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 10-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 11-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 12-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 13-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 14-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 15-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 16-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 17-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 18-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 19-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 20-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 21-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 22-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 23-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 24-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 25-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 26-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 27-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 28-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 29-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 30-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 31-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 32-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 33-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 34-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 35-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 36-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 37-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 38-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 39-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 41-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 42-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 43-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 44-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 45-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 46-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 47-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 48-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 49-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 51-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 52-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 53-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 54-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 55-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 56-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 57-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 58-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 59-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 61-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 62-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 63-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 64-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 65-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 66-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 67-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 68-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 69-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 71-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 72-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 73-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 74-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 75-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 76-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 77-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 78-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 79-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 81-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 82-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 83-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 84-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 85-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 86-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 87-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 88-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 89-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 91-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 92-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 93-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 94-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 95-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 96-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 97-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 98-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 99-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 101-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 102-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 103-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 104-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 105-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 106-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 107-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 108-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 109-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 110-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 112-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 113-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 114-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 115-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 116-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 117-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 118-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 119-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 120-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 121-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 122-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 123-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 124-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 125-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 126-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 127-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 128-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 129-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 131-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 132-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 133-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 134-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 135-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 136-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 137-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 138-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 139-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 140-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 141-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 142-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 143-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 144-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 145-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 146-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 147-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 148-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 149-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 151-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 152-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 153-compressed.jpg",
+    "rotato_frames_compressed/animated-landing-page-video Frame 154-compressed.jpg",
+  ];
 
-//SCROLLMAGIC
-const controller = new ScrollMagic.Controller();
+  // TweenMax can tween any property of any object. We use this object to cycle through the array
+  var obj = { curImg: 0 };
+  const intro = document.querySelector(".intro");
+  const trigger = document.querySelector(".trigger");
 
-//Scenes
-let scene = new ScrollMagic.Scene({
-  duration: 3500,
-  triggerElement: intro,
-  triggerHook: 0
-})
-  // .addIndicators()
-  .setPin(intro)
-  .addTo(controller);
-//Text Animation
-const textAnim = TweenMax.fromTo(text, 2, { opacity: 1 }, { opacity: 0 });
+  // create tween
+  var tween = TweenMax.to(obj, 0.5,
+    {
+      curImg: images.length - 1,	// animate propery curImg to number of images
+      roundProps: "curImg",				// only integers so it can be used as an array index
+      repeat: 3,									// repeat 3 times
+      immediateRender: true,			// load first image automatically
+      ease: Linear.easeNone,			// show every image the same ammount of time
+      onUpdate: function () {
+        document.getElementById("myimg").src = images[obj.curImg];
+        // document.querySelector(".myimg").attr("src", images[obj.curImg]); // set the image source
+      }
+    }
+  );
 
-let scene2 = new ScrollMagic.Scene({
-  duration: 2000,
-  triggerElement: intro,
-  triggerHook: 0
-})
-  .setTween(textAnim)
-  .addTo(controller);
+  // init controller
+  var controller = new ScrollMagic.Controller();
+  const text = intro.querySelector("h1");
+  const end = intro.querySelector("h1");
 
-//Video Animation
-let accelamount = 0.1;
-let scrollpos = 0;
-let delay = 0;
+  // build scene
+  var scene = new ScrollMagic.Scene({ triggerElement: intro, duration: 3500, triggerHook: 0 })
+    .setTween(tween)
+    .setPin(intro)
+    .addTo(controller);
 
-scene.on("update", e => {
-  scrollpos = e.scrollPos / 1000;
-});
+  const textAnim = TweenMax.fromTo(text, 2, { opacity: 1 }, { opacity: 0 });
 
-setInterval(() => {
-  delay += (scrollpos - delay) * accelamount;
-  video.currentTime = delay;
-}, 33.3);
+  let scene2 = new ScrollMagic.Scene({
+    duration: 2000,
+    triggerElement: intro,
+    triggerHook: 0
+  })
+    .setTween(textAnim)
+    .addTo(controller);
+
+  // handle form change
+  // $("form.move input[name=duration]:radio").change(function () {
+  //   scene.duration($(this).val());
+  // });
+}
